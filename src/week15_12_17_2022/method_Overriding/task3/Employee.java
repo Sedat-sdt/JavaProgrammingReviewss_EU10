611e847bbfb3d8cbb80ebcdd6583eb6961439471
Sedat Maden <madensedat05@gmail.com>
2022-12-17 20:52:29 +0300

    day 15 task2

--- src/week15_12_17_2022/method_Overriding/task3/Employee.java
+++ src/week15_12_17_2022/method_Overriding/task3/Employee.java
@@ -2,6 +2,13 @@ package week15_12_17_2022.method_Overriding.task3;
 
 public class Employee {
 
+    private int baseSalary=10000;
+
+    public int salary(){
+        return  baseSalary;
+
+    }
+
 
 
 
---
+++ src/week15_12_17_2022/method_Overriding/task3/TestSalary.java
@@ -0,0 +1,8 @@
+package week15_12_17_2022.method_Overriding.task3;
+
+public class TestSalary {
+    public static void main(String[] args) {
+        Manager manager=new Manager();
+        System.out.println(manager.salary());//30000
+    }
+}
